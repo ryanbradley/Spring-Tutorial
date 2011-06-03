026cb15973483fd517de8dc0bc627f656256401c
Ryan Bradley <rbradley@redhat.com>
2011-06-03 13:37:26 -0400

    All dependencies have been migrated to the pom.xml file and updated to the Spring 3.0.5.RELEASE framework (where applicable).  The project no longer contains any imported libraries in WEB-INF/lib.  The next step is to update files using annotations.

--- tutorial/db/server.sh
+++ tutorial/db/server.sh
@@ -1,4 +1,4 @@
-java -classpath ../src/main/webapp/WEB-INF/lib/hsqldb.jar org.hsqldb.Server -database test
+java -classpath /home/rbradley/.m2/repository/hsqldb/hsqldb/1.8.0.10/hsqldb-1.8.0.10.jar org.hsqldb.Server -database test
 
-java -jar ../WebContent/WEB-INF/lib/hsqldb.jar --inlinerc url=jdbc:hsqldb:hsql://localhost,user=sa,password= create_products.sql load_data.sql
+#java -jar ~/.m2/repository/hsqldb/hsqldb/1.8.0.10/hsqldb-1.8.0.10.jar --inlinerc url=jdbc:hsqldb:hsql://localhost,user=sa,password= create_products.sql load_data.sql
 
