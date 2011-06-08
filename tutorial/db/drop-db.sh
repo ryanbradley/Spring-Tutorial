5eb5b7c6cde9b90101e6226cbb92f7403f7cec78
Ryan Bradley <rbradley@redhat.com>
2011-06-08 13:44:08 -0400

    Application now uses a managed datasource hosted on the application server as opposed to a database which must be started by the same host machine that runs the application.

--- tutorial/db/drop-db.sh
+++ tutorial/db/drop-db.sh
@@ -1,1 +1,3 @@
-java -jar ~/.m2/repository/hsqldb/hsqldb/1.8.0.10/hsqldb-1.8.0.10.jar --inlinerc url=jdbc:hsqldb:hsql://localhost,user=sa,password= --sql "drop table products if exists;"
+java -jar ~/.m2/repository/hsqldb/hsqldb/1.8.0.10/hsqldb-1.8.0.10.jar --inlinerc url=jdbc:hsqldb:hsql://localhost/tutorialdb,user=sa,password= --sql "drop table products if exists;"
+
+
--- tutorial/db/init-db.sh
+++ tutorial/db/init-db.sh
@@ -1,1 +1,1 @@
-java -jar ~/.m2/repository/hsqldb/hsqldb/1.8.0.10/hsqldb-1.8.0.10.jar --inlinerc url=jdbc:hsqldb:hsql://localhost,user=sa,password= create_products.sql load_data.sql
+java -jar ~/.m2/repository/hsqldb/hsqldb/1.8.0.10/hsqldb-1.8.0.10.jar --inlinerc url=jdbc:hsqldb:hsql://localhost/tutorialdb,user=sa,password= create_products.sql load_data.sql
